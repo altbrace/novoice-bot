e554c3ed8fca11298dc105bcf2a3a23c27046f3e
altbrace <dim203286@gmail.com>
2020-07-19 02:23:31 +0700

    Added support for transcribing long audio files (+debug info)

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import os
 import redis
 
 
-def google_stt(raw):
+def google_stt(raw, duration):
     client = speech_v1p1beta1.SpeechClient.from_service_account_json('google-credentials.json')
 
     encoding = enums.RecognitionConfig.AudioEncoding.MP3
@@ -26,9 +26,13 @@ def google_stt(raw):
 
     print("Voice recognition initiated...")
     t0 = time.time()
-    response = client.recognize(config, audio)
+
+    if duration <= 60:
+        response = client.recognize(config, audio)
+    else:
+        response = client.long_running_recognize(config, audio)
+
     for result in response.results:
-        # First alternative is the most probable result
         alternative = result.alternatives[0]
         seconds = time.time() - t0
         print(f"Voice recognition completed in {round(seconds)} sec.\n"
@@ -108,14 +112,16 @@ class Bot:
 
     def start(self):
         for event in self.bot_long_poll.listen():
+            print(event)
             attachments = event.object.attachments
             if event.type == VkBotEventType.MESSAGE_NEW and attachments:
 
                 for attachment in attachments:
                     if attachment['type'] == 'audio_message':
                         response = self.session.get(attachment['audio_message']['link_mp3'])
+                        duration = attachment['audio_message']['duration']
                         raw = response.content
-                        transcribed = google_stt(raw)
+                        transcribed = google_stt(raw, duration)
                         if not transcribed:
                             transcribed = "[неразборчиво]"
                         self.send_msg(event.object.peer_id, event.object.id, transcribed)
